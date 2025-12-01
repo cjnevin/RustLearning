ae8b35f5e8218e12835527160caa50d240b5d533
cjnevin <cjnevin@pm.me>
2025-12-01 15:24:19 +1000

    Chapter 3: Data types

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,9 +1,16 @@
 const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
+mod invalid_index;
+
 fn main() {
     mutability();
     println!("Three hours in seconds: {}", THREE_HOURS_IN_SECONDS);
     shadowing();
+    tuples();
+    type_inferred_array();
+    typed_array();
+    repeated_item_array();
+    invalid_index::invalid_array();
 }
 
 fn mutability() {
@@ -21,4 +28,24 @@ fn shadowing() {
         println!("The value of x in the inner scope is: {x}");
     }
     println!("The value of x is: {x}");
+}
+
+fn tuples() {
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    println!("{} {} {}", tup.0, tup.1, tup.2);
+}
+
+fn type_inferred_array() {
+    let a = [1, 2, 3, 4, 5];
+    println!("{}", a[4]);
+}
+
+fn typed_array() {
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{}", a[4]);
+}
+
+fn repeated_item_array() {
+    let a = [3; 5];
+    println!("{}", a[4]);
 }
