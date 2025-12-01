d7640b7a6be9eec7a99f7cd839d49db68b0fab5c
cjnevin <cjnevin@pm.me>
2025-12-01 18:54:38 +1000

    Chapter 5: Methods

--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -15,6 +15,27 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     user_mutation();
 
@@ -23,6 +44,7 @@ fn main() {
     let _subject = AlwaysEqual;
 
     area_calculations();
+    rectangle_can_contain();
 }
 
 fn user_mutation() {
@@ -84,6 +106,15 @@ fn area_calculations() {
         height: 50,
     };
     dbg!(&rect3);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect3.area()
+    );
+
+    if rect3.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect3.width);
+    }
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -96,4 +127,22 @@ fn area_tuple(dimensions: (u32, u32)) -> u32 {
 
 fn area_struct(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
+}
+
+fn rectangle_can_contain() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
