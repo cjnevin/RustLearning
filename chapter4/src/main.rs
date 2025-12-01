f557771c9fbf12c0ac8298c6bff08f3a24fa432e
cjnevin <cjnevin@pm.me>
2025-12-01 16:21:24 +1000

    Chapter 4: Slice Type

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     ownership_give_example();
     returning_ownership_of_parameters();
     borrowing();
+    slicing();
 }
 
 fn string_mutation() {
@@ -141,4 +142,81 @@ fn borrowing_calculate_length(s: &String) -> usize {
 
 fn change(some_string: &mut String) {
     some_string.push_str(", world");
+}
+
+fn slicing() {
+    length_slicing();
+    exact_slicing();
+    improved_slicing();
+}
+
+fn length_slicing() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // word will get the value 5
+    println!("{}", &s[(word + 1)..]);
+
+    s.clear(); // this empties the String, making it equal to ""
+
+    // word still has the value 5 here, but s no longer has any content that we
+    // could meaningfully use with the value 5, so word is now totally invalid!
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
+}
+
+fn exact_slicing() {
+    let s2 = String::from("hello world");
+    let hello = &s2[0..5];
+    let world = &s2[6..11];
+    println!("{hello} {world}");
+}
+
+fn improved_slicing() {
+    let my_string = String::from("hello world");
+
+    // `first_word_improved` works on slices of `String`s, whether partial or whole.
+    let word = first_word_improved(&my_string[0..6]);
+    let word = first_word_improved(&my_string[..]);
+    // `first_word_improved` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s.
+    let word = first_word_improved(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word_improved` works on slices of string literals, whether partial or
+    // whole.
+    let word = first_word_improved(&my_string_literal[0..6]);
+    let word = first_word_improved(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word_improved(my_string_literal);
+}
+
+fn first_word_improved(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn slicing() {
+        let a = [1, 2, 3, 4, 5];
+        let slice = &a[1..3];
+        assert_eq!(slice, &[2, 3]);
+    }
 }
