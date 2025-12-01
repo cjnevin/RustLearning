ffbf55896f2aaa9ab2ab949acce29c57bf462867
cjnevin <cjnevin@pm.me>
2025-12-01 18:31:42 +1000

    Chapter 4: Cleanup warnings.

--- chapter4/src/main.rs
+++ chapter4/src/main.rs
@@ -65,12 +65,12 @@ fn makes_copy(some_integer: i32) {
 } // Here, some_integer goes out of scope. Nothing special happens.
 
 fn ownership_give_example() {
-    let s1 = gives_ownership(); // gives_ownership moves its return
+    let _s1 = gives_ownership(); // gives_ownership moves its return
     // value into s1
 
     let s2 = String::from("hello"); // s2 comes into scope
 
-    let s3 = takes_and_gives_back(s2); // s2 is moved into
+    let _s3 = takes_and_gives_back(s2); // s2 is moved into
     // takes_and_gives_back, which also
     // moves its return value into s3
 } // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
@@ -151,6 +151,8 @@ fn slicing() {
 }
 
 fn length_slicing() {
+    println!("Length slicing");
+
     let mut s = String::from("hello world");
 
     let word = first_word(&s); // word will get the value 5
@@ -173,6 +175,8 @@ fn first_word(s: &String) -> usize {
 }
 
 fn exact_slicing() {
+    println!("Range slicing");
+
     let s2 = String::from("hello world");
     let hello = &s2[0..5];
     let world = &s2[6..11];
@@ -181,26 +185,34 @@ fn exact_slicing() {
 
 fn improved_slicing() {
     let my_string = String::from("hello world");
+    println!("Improved slicing");
 
     // `first_word_improved` works on slices of `String`s, whether partial or whole.
     let word = first_word_improved(&my_string[0..6]);
+    println!("{word}");
     let word = first_word_improved(&my_string[..]);
+    println!("{word}");
     // `first_word_improved` also works on references to `String`s, which are equivalent
     // to whole slices of `String`s.
     let word = first_word_improved(&my_string);
+    println!("{word}");
 
     let my_string_literal = "hello world";
 
     // `first_word_improved` works on slices of string literals, whether partial or
     // whole.
     let word = first_word_improved(&my_string_literal[0..6]);
+    println!("{word}");
     let word = first_word_improved(&my_string_literal[..]);
+    println!("{word}");
 
     // Because string literals *are* string slices already,
     // this works too, without the slice syntax!
     let word = first_word_improved(my_string_literal);
+    println!("{word}");
 }
 
+// &str is a slice of a String
 fn first_word_improved(s: &str) -> &str {
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
