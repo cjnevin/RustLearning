44e6a50b0de8bdf6aecdd01f43fe25a5c5c57f3c
cjnevin <cjnevin@pm.me>
2025-12-01 18:50:03 +1000

    Chapter 5: Example

--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -9,7 +9,23 @@ struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
 struct AlwaysEqual;
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
+    user_mutation();
+
+    let _black = Color(0, 0, 0);
+    let _origin = Point(0, 0, 0);
+    let _subject = AlwaysEqual;
+
+    area_calculations();
+}
+
+fn user_mutation() {
     let mut user1 = User {
         active: true,
         username: String::from("someusername123"),
@@ -34,8 +50,50 @@ fn main() {
         "{} {} {} {}",
         user2.active, user2.username, user2.email, user2.sign_in_count
     );
+}
 
-    let _black = Color(0, 0, 0);
-    let _origin = Point(0, 0, 0);
-    let _subject = AlwaysEqual;
+fn area_calculations() {
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+
+    let rect1 = (30, 50);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_tuple(rect1)
+    );
+
+    let rect2 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_struct(&rect2)
+    );
+
+    println!("Rect is {rect2:?}");
+
+    let scale = 2;
+    let rect3 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+    dbg!(&rect3);
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area_tuple(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
