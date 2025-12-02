ae0c94e2110751761b9d14e0264269dddc55755d
cjnevin <cjnevin@pm.me>
2025-12-02 18:05:08 +1000

    Chapter 2 + 5: Added unit tests

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -40,8 +40,10 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {value}.");
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {value}.");
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {value}.");
         }
 
         Guess { value }
@@ -49,5 +51,22 @@ impl Guess {
 
     pub fn value(&self) -> i32 {
         self.value
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    #[should_panic(expected = "greater than or equal to 1")]
+    fn less_than_1() {
+        Guess::new(0);
     }
 }
