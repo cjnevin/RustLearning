e8f360aebf8e621a8c9213d0f349b46936e07b0d
cjnevin <cjnevin@pm.me>
2025-12-02 12:34:52 +1000

    Chapter 6: Match Control Flow

--- chapter6/src/main.rs
+++ chapter6/src/main.rs
@@ -12,6 +12,35 @@ enum Message {
     ChangeColor(i32, i32, i32),
 }
 
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
 impl Message {
     fn call(&self) {
         // method body would be defined here
@@ -22,6 +51,7 @@ impl Message {
 fn main() {
     ip_address();
     message_call();
+    match_control_flow();
 }
 
 fn ip_address() {
@@ -44,4 +74,11 @@ fn message_call() {
 
     let c = Message::ChangeColor(255, 40, 40);
     c.call();
+}
+
+fn match_control_flow() {
+    println!("{}", value_in_cents(Coin::Penny));
+    println!("{}", value_in_cents(Coin::Nickel));
+    println!("{}", value_in_cents(Coin::Dime));
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));
 }
