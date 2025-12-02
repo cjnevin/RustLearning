a243fd6e3a335de06e915202b81328361a616212
cjnevin <cjnevin@pm.me>
2025-12-02 17:59:58 +1000

    Chapter 10: Lifetimes

---
+++ chapter10/lifetimes/src/main.rs
@@ -0,0 +1,67 @@
+use std::fmt::Display;
+
+fn main() {
+    borrowing();
+    longest_string();
+    important_excerpt();
+}
+
+fn borrowing() {
+    let r;
+
+    let x = 5;
+    r = &x;
+
+    println!("r: {r}");
+}
+
+fn longest_string() {
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {result}");
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() { x } else { y }
+}
+
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {announcement}");
+        self.part
+    }
+}
+
+fn important_excerpt() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("{i:?}");
+    println!("{}", i.level());
+    println!("{}", i.announce_and_return_part("Some announcement"));
+    longest_with_an_announcement("first", "sec", "Some announcement");
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() { x } else { y }
+}
