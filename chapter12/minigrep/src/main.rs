45f354c625cc28cb06277b4a644b3ae70692fb12
cjnevin <cjnevin@pm.me>
2025-12-03 09:58:00 +1000

    Chapter 12: Standard Error logging

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -4,16 +4,13 @@ use std::{env, error::Error, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
