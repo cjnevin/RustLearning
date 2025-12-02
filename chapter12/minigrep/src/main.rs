d5f770495142b1235424e53a3bc68d41b699ac6d
cjnevin <cjnevin@pm.me>
2025-12-03 09:56:23 +1000

    Chapter 12: Search - Case Insensitive

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -21,7 +21,13 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search_case_insensitive(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -29,8 +35,9 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -42,6 +49,12 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
