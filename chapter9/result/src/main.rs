a7e3a59752822aa7ec460d075948df4cac2bc37b
cjnevin <cjnevin@pm.me>
2025-12-02 16:25:00 +1000

    Chapter 9: Result

---
+++ chapter9/result/src/main.rs
@@ -0,0 +1,73 @@
+use std::error::Error;
+use std::fs::{self, File};
+use std::io::{self, ErrorKind, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let _ = File::open("hello.txt")?;
+
+    with_match();
+    without_match();
+    println!("{:?}", read_username_from_file());
+    println!("{:?}", read_username_from_file_shorter());
+    println!("{:?}", read_username_from_file_shortest());
+    println!("{:?}", last_char_of_first_line("test\nblah"));
+    // with_panic();
+
+    Ok(())
+}
+
+fn with_panic() {
+    let greeting_file = File::open("hello2.txt").unwrap();
+    println!("{:?}", greeting_file);
+}
+
+fn with_match() {
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {e:?}"),
+            },
+            _ => {
+                panic!("Problem opening the file: {error:?}");
+            }
+        },
+    };
+    println!("{:?}", greeting_file);
+}
+
+fn without_match() {
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {error:?}");
+            })
+        } else {
+            panic!("Problem opening the file: {error:?}");
+        }
+    });
+    println!("{:?}", greeting_file);
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_shorter() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_shortest() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
